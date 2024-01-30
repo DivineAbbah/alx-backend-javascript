b9f76464b2717acc91e405d8d2a6eaf50a1f36e2
Divine Abbah <abbahattamah@gmail.com>
2024-01-30 17:17:02 +0000

    Update

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Display messages on the STDOUT.
  * @param {String} msg message to display.
- * @author Divine Abbah<https://github.com/DivineAbbah>
+ * @author Divine Abbah <https://github.com/DivineAbbah>
  */
 const displayMessage = (msg) => {
   console.log(msg);
