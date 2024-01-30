202814b26c3d7c01d13cfe59f97fd1fed1d6d816
Divine Abbah <abbahattamah@gmail.com>
2024-01-30 16:18:03 +0000

    Node file operations

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display messages on the STDOUT.
+ * @param {String} msg message to display.
+ * @author Divine Abbah<https://github.com/DivineAbbah>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
